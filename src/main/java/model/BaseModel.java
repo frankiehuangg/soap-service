052a86410461c7d40f04f50cfcbf110c4a3120f5
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-11-11 14:55:19 +0700

    feat: models for Base, APIKey, and Logging

--- src/main/java/model/BaseModel.java
+++ src/main/java/model/BaseModel.java
@@ -1,4 +1,8 @@
 package model;
 
-public class BaseModel {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class BaseModel {
+    public abstract void constructFromSQL(ResultSet rs) throws SQLException;
 }
