12e95b76dcbad6c302d98098ccc01f8da7c206ae
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-11-17 12:40:24 +0700

    feat: Block model soap service

--- src/main/java/model/Block.java
+++ src/main/java/model/Block.java
@@ -1,4 +1,26 @@
 package model;
 
-public class Block {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement
+public class Block extends BaseModel {
+    private Integer blockingUserId;
+    private Integer blockedUserId;
+
+    @Override
+    public void constructFromSQL(ResultSet rs) throws SQLException {
+        this.blockingUserId = rs.getInt("blocking_user_id");
+        this.blockedUserId  = rs.getInt("blocked_user_id");
+    }
 }
